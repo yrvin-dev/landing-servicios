568a6e7bd9314562ed17a8c4d514ff49f81cf01a
yrvin-dev <yrvin.dev@gmail.com>
2026-07-05 13:36:58 -0500

    fix: actualizar CORS con dominio real de Vercel

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -2,16 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:4321',
-        'https://landing-servicios.vercel.app',
-      ],
-      headers: '*',
-    },
+ {
+  name: 'strapi::cors',
+  config: {
+    origin: [
+      'http://localhost:4321',
+      'https://landing-servicios-sage.vercel.app',
+    ],
+    headers: '*',
   },
+},
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
